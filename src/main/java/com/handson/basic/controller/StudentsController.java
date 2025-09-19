fdd35012f3a71d47ffd8c9be2d0c044f452f13f7
Dolev Ben Shushan <dolev532@gmail.com>
2025-09-19 20:09:09 +0300

    with actuator & global exception

--- src/main/java/com/handson/basic/controller/StudentsController.java
+++ src/main/java/com/handson/basic/controller/StudentsController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.handson.basic.model.*;
 import com.handson.basic.repo.StudentService;
 import com.handson.basic.util.AWSService;
+import com.handson.basic.util.HandsonException;
 import com.handson.basic.util.SmsService;
 import org.apache.commons.collections4.IteratorUtils;
 import org.joda.time.LocalDate;
@@ -105,7 +106,7 @@ public class StudentsController {
     public ResponseEntity<?> uploadStudentImage(@PathVariable Long id,  @RequestParam("image") MultipartFile image)
     {
         Optional<Student> dbStudent = studentService.findById(id);
-        if (dbStudent.isEmpty()) throw new RuntimeException("Student with id: " + id + " not found");
+        if (dbStudent.isEmpty()) throw new HandsonException("Student with id: " + id + " not found");
         String bucketPath = "apps/niv/student-" +  id + ".png" ;
         awsService.putInBucket(image, bucketPath);
         dbStudent.get().setProfilePicture(bucketPath);
@@ -125,7 +126,7 @@ public class StudentsController {
     public ResponseEntity<?> updateStudent(@PathVariable Long id, @RequestBody StudentIn student)
     {
         Optional<Student> dbStudent = studentService.findById(id);
-        if (dbStudent.isEmpty()) throw new RuntimeException("Student with id: " + id + " not found");
+        if (dbStudent.isEmpty()) throw new HandsonException("Student with id: " + id + " not found");
         student.updateStudent(dbStudent.get());
         Student updatedStudent = studentService.save(dbStudent.get());
         return new ResponseEntity<>(updatedStudent, HttpStatus.OK);
@@ -135,7 +136,7 @@ public class StudentsController {
     public ResponseEntity<?> deleteStudent(@PathVariable Long id)
     {
         Optional<Student> dbStudent = studentService.findById(id);
-        if (dbStudent.isEmpty()) throw new RuntimeException("Student with id: " + id + " not found");
+        if (dbStudent.isEmpty()) throw new HandsonException("Student with id: " + id + " not found");
         studentService.delete(dbStudent.get());
         return new ResponseEntity<>("DELETED", HttpStatus.OK);
     }
